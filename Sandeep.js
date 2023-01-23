14b89279185d313d6e17ca21c6950a43e97120d7
Sandeep Kumar Rai <raisandeep.dev@gmail.com>
2023-01-24 00:32:00 +0530

    Done

--- Sandeep.js
+++ Sandeep.js
@@ -1,6 +1,14 @@
 
 
-$(document).ready(function () {
-  $(".u-animation-once").addClass("bounceIn");
-  setTimeout(function(){$(".u-animation-once").removeClass("bounceIn")},2000);
-});
+// $(document).ready(function () {
+//   $(".u-animation-once").addClass("bounceIn");
+//   setTimeout(function(){$(".u-animation-once").removeClass("bounceIn")},2000);
+// });
+
+// $(".u-animation-once").animate(
+//   { animationName: "bounceIn", duration: 2000 },
+//   function () {
+//     //callback function
+//     $(this).stop();
+//   }
+// );
